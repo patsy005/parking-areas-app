654123d0507013bcd4fda6ed14607a1152670a57
Patrycja <patrycja.zawadzka95@gmail.com>
2024-11-12 19:19:28 +0000

    fixed modal

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,7 +1,5 @@
 import { ComponentPropsWithoutRef } from 'react'
 import ReactDOM from 'react-dom'
-import { useDispatch } from 'react-redux'
-import { closeModal } from '../../slices/ModalSlice'
 
 type ModalProps = {
 	isOpen: boolean
@@ -9,16 +7,11 @@ type ModalProps = {
 } & ComponentPropsWithoutRef<'div'>
 
 export default function Modal({ isOpen, children, ...props }: ModalProps) {
-	const dispatch = useDispatch()
 
 	if (!isOpen) return null
 
-	const handleOverlayClick = () => {
-		dispatch(closeModal())
-	}
-
 	return ReactDOM.createPortal(
-		<div className="modal__overlay" {...props} onClick={handleOverlayClick}>
+		<div className="modal__overlay" {...props}>
 			<div className="modal__content">{children}</div>
 		</div>,
 		document.body
